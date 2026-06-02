a837fd5137e46de2edb0ebcd5b0343f2c74a8497
Bikash Malla <bikashmalla80@gmail.com>
2026-06-02 07:39:55 -0500

    Solved Stack pattern  problem

--- DSA.py
+++ DSA.py
@@ -419,17 +419,73 @@ Output: False
 """
 
 
-def ransome_note(ransomNote, magazine):
-    count = {}
+# def ransome_note(ransomNote, magazine):
+#     count = {}
 
 
-    for char in magazine:
-        count[char] = count.get(char, 0) + 1
+#     for char in magazine:
+#         count[char] = count.get(char, 0) + 1 # counting pattern -- most important line! Yesma count.get(char, 0) == yes if char xa vnya 1 dinxa xaina vnya 0
     
-    for char in ransomNote:
-        if char not in count or count[char] == 0:
-            return False
-        count[char] -= 1
-    return True
+#     for char in ransomNote:
+#         if char not in count or count[char] == 0: # or mean kunai neh euta condition meet vaya pugxa
+#             return False
+#         count[char] -= 1
+#     return True
 
-print(ransome_note("a", "b"))
+# print(ransome_note("a", "b"))
+
+
+# 3. Stack Pattern
+"""
+🟢 Easy — Min Stack (Stack Pattern)
+Problem:
+Design a stack that supports these operations in O(1) time:
+
+push(val) — push element onto stack
+pop() — remove top element
+top() — get top element
+getMin() — get minimum element in stack
+
+Example:
+MinStack minStack = MinStack()
+minStack.push(-2)
+minStack.push(0)
+minStack.push(-3)
+minStack.getMin() → -3
+minStack.pop()
+minStack.top()    → 0
+minStack.getMin() → -2
+"""
+
+class Minstack:
+    def __init__(self):
+        self.stack = []
+        self.min_stack = []
+
+    def push(self, val):
+        self.stack.append(val)
+        if not self.min_stack or val <= self.min_stack[-1]:
+            self.min_stack.append(val)
+    def pop(self):
+        if self.stack[-1] == self.min_stack[-1]:
+            self.min_stack.pop()
+        self.stack.pop()
+
+    def top(self):
+        return self.stack[-1]
+    
+    def getMin(self):
+        return self.min_stack[-1]
+    
+
+
+
+
+minStack = Minstack()
+minStack.push(-2)
+minStack.push(0)
+minStack.push(-3)
+print(minStack.getMin())  # -3
+minStack.pop()
+print(minStack.top())     # 0
+print(minStack.getMin())  # -2
